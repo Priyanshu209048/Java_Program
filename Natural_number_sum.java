71cef72eea8eb176b7ee6dd5f6127b18ec78653b
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2022-09-23 20:57:56 +0530

    Add files via upload

---
+++ Natural_number_sum.java
@@ -0,0 +1,11 @@
+package com.practice;
+
+public class Natural_number_sum {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i=1; i<=50; i++) {
+            count += i;
+        }
+        System.out.println(count);
+    }
+}
