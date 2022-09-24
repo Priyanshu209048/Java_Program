eb0ff44b9013bb0c6f5e1105c61387183cdc88a9
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2022-09-24 20:57:04 +0530

    Add files via upload

---
+++ Guess_The_Number_Game.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class Game{
+    private int num, inputNum, noOfGuesses = 0;
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
+    }
+    public int getNoOfGuesses(){
+        return noOfGuesses;
+    }
+    Game(){
+        Random rand = new Random();
+        this.num = rand.nextInt(1,100);
+    }
+    void takeUserInput(){
+        System.out.println("Guess the number:");
+        Scanner sc = new Scanner(System.in);
+        inputNum = sc.nextInt();
+    }
+    boolean isCorrectNumber(){
+        noOfGuesses++;
+        if(inputNum == num){
+            System.out.println("Yes you guess it right, it was "+num+" \nYou guessed it in "+noOfGuesses+" attempts");
+            return true;
+        }
+        else if (inputNum<num){
+            System.out.println("To Low...");
+        }
+        else if (inputNum>num){
+            System.out.println("To High...");
+        }
+        return false;
+    }
+}
+
+public class Guess_The_Number_Game {
+    public static void main(String[] args) {
+        Game g = new Game();
+        while(true){
+            g.takeUserInput();
+            boolean b = g.isCorrectNumber();
+        }
+    }
+}
