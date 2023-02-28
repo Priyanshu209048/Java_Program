a9e5fca1916f6380f2c9f0c9a41b1ec7d323845a
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-02-28 23:36:47 +0530

    Add files via upload

---
+++ Nested_Try_Catch.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Nested_Try_Catch {
+    public static void main(String[] args) {
+        int [] marks = {1,5,7};
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while(flag) {
+            System.out.println("Enter the value of index");
+            int ind = sc.nextInt();
+            try {
+                System.out.println("Welcome to this code");
+                try {
+                    System.out.println(marks[ind]);
+                    flag = false;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Sorry this index does not exist");
+                    System.out.println("Exception in level 2");
+                }
+            } catch (Exception e) {
+                System.out.println("Exception in level 1");
+            }
+        }
+        System.out.println("Thanks for using this program");
+    }
+}
