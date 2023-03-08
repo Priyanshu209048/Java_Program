9e69514cd4c27cecfafc7ec0b54caefb490df658
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-03-08 21:41:34 +0530

    Add files via upload

---
+++ String_Method.java
@@ -0,0 +1,49 @@
+public class String_Method {
+    public static void main(String[] args) {
+        String name = "Priya";
+        //System.out.println(name);
+
+        //int value = name.length();
+        //System.out.println(value);
+
+        //String lstring = name.toLowerCase();
+        //System.out.println(lstring);
+
+        //String ustring = name.toUpperCase();
+        //System.out.println(ustring);
+
+        //String noTrime = "   Priya   ";
+        //System.out.println(noTrime);
+        //String trime = noTrime.trim();
+        //System.out.println(trime);
+
+        //System.out.println(name.substring(1));
+        //System.out.println(name.substring(1,4));
+
+        //System.out.println(name.replace('r','i'));
+        //System.out.println(name.replace("ri","i"));
+
+        //System.out.println(name.startsWith("Pri"));
+        //System.out.println(name.endsWith("ee"));
+
+        //System.out.println(name.charAt(4));
+
+        String name2 = "Priyaiya";
+        //System.out.println(name2.indexOf("iya"));
+        //System.out.println(name2.indexOf("iya", 4));
+        //System.out.println(name2.lastIndexOf("iya"));
+        //System.out.println(name2.lastIndexOf("iya",4));
+
+        //System.out.println(name.equals("PriYA"));
+        //System.out.println(name.equalsIgnoreCase("PriYA"));
+
+        //System.out.println(name.compareTo(name2));
+
+        //System.out.println(name.concat(name2));
+
+        //System.out.println(String.valueOf(name));
+
+        System.out.println("Welcome to \"Geeks for Geeks\"");
+        System.out.println("Print this \\ statement");
+    }
+}
