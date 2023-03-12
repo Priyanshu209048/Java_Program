85a012d0b98f14b73c1509ccdfdcfc26c83b5481
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-03-12 22:34:50 +0530

    Add files via upload

---
+++ Time_Zone.java
@@ -0,0 +1,10 @@
+import java.util.TimeZone;
+
+public class Time_Zone {
+    public static void main(String[] args) {
+        System.out.println(TimeZone.getAvailableIDs()[0]);
+        System.out.println(TimeZone.getAvailableIDs()[1]);
+        System.out.println(TimeZone.getAvailableIDs()[2]);
+        System.out.println(TimeZone.getAvailableIDs()[3]);
+    }
+}
