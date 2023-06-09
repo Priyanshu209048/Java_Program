f9ed1f57761ef1bf204c29f998bc358a18e816ea
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-06-09 21:56:18 +0530

    Add files via upload

---
+++ Calender_Class/add_Method.java
@@ -0,0 +1,15 @@
+package Calender_Class;
+import java.util.*;
+
+public class add_Method {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current date is : " + c.getTime());
+        c.add(Calendar.YEAR, 4);
+        System.out.println("After 4 years  : "+ c.getTime());
+        c.add(Calendar.YEAR, -12);
+        System.out.println("Before 12 years  : "+ c.getTime());
+        c.add(Calendar.MONTH,2);
+        System.out.println("After 2 months  : "+ c.getTime());
+    }
+}
