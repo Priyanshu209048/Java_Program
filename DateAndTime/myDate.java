0b056120106a3e80357d30e3904f2232e64c4f5d
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-06-13 22:47:47 +0530

    Add files via upload

---
+++ DateAndTime/myDate.java
@@ -0,0 +1,9 @@
+package DateAndTime;
+
+public class myDate {
+    public static void main(String[] args) {
+        System.out.println("The maximum value of long is :" + Long.MAX_VALUE);
+        System.out.println("Year spent from January 1st 1970 to till now");
+        System.out.println(System.currentTimeMillis()/1000/3600/24/365);
+    }
+}
