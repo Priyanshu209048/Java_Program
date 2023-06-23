85e0b6908656af00382677eeb8e26cb3cc765ab2
PriyanshuBaral <72007362+Priyanshu209048@users.noreply.github.com>
2023-06-23 20:56:35 +0530

    Add files via upload

---
+++ Java_Time_API/Local_Time.java
@@ -0,0 +1,9 @@
+package Java_Time_API;
+import java.time.LocalTime;
+
+public class Local_Time {
+    public static void main(String[] args) {
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+    }
+}
